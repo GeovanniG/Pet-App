4e7e91b64617eeee9832ea2fc868d3906629a137
GeovanniG <32276348+GeovanniG@users.noreply.github.com>
2019-07-28 07:32:44 -0700

    Updates

---
+++ precache-manifest.f27054923907d507904c911136f2c621.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "205fff396ca0e10bd720c5ad6bdc9105",
+    "url": "/Pet-App/index.html"
+  },
+  {
+    "revision": "c75d54e0c3ff7570e82b",
+    "url": "/Pet-App/static/css/main.cb9134ac.chunk.css"
+  },
+  {
+    "revision": "47660a4a93b12ab6318c",
+    "url": "/Pet-App/static/js/2.da936911.chunk.js"
+  },
+  {
+    "revision": "c75d54e0c3ff7570e82b",
+    "url": "/Pet-App/static/js/main.1216a983.chunk.js"
+  },
+  {
+    "revision": "de50e3e87cf99d8dec31",
+    "url": "/Pet-App/static/js/runtime~main.bbd8f0f2.js"
+  },
+  {
+    "revision": "b57234b7479efafd2822ac86ff21d0f8",
+    "url": "/Pet-App/static/media/header_bg.b57234b7.jpg"
+  }
+]);
